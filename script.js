0b4bbaa6b7bf61dce71fd6813c3de0b0da85fd9e
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-25 20:06:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const makeChange = (c) => {
-  // your name here
+const makeChange = (amount) => {
+
+	 const change = {
+	    q: 0, // quarters
+	    d: 0, // dimes
+	    n: 0, // nickels
+	    p: 0  // pennies
+	};
+
+  // Quarters
+  change.q = Math.floor(amount / 25);
+  amount %= 25;
+
+  // Dimes
+  change.d = Math.floor(amount / 10);
+  amount %= 10;
+
+  // Nickels
+  change.n = Math.floor(amount / 5);
+  amount %= 5;
+
+  // Pennies
+  change.p = amount; // Remaining amount is all pennies
+
+  return change;
+	
+  
 };
 
 // Do not the change the code below
